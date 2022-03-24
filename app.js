72eae095eec4d71cc71909c8164925160941b626
lbferrand <lbferrand@gmail.com>
2022-03-24 16:33:56 -0400

    Added styles and image to index html page

--- app.js
+++ app.js
@@ -15,5 +15,23 @@ function PageTransitions() {
       this.className += " active-btn";
     });
   }
+
+  //Sections Active class
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      //remove selected from the other btns
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+      //hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
 }
 PageTransitions();
