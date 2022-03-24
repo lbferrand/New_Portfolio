b6402ee7884d4feaec280a0ae416bcca5b695973
lbferrand <lbferrand@gmail.com>
2022-03-23 23:17:16 -0400

    Added style css and created app.js

---
+++ styles/app.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controls"); //section where the buttons are located
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+function PageTransitions() {
+  //Button click active class
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener("click", () => {
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].classList = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
+      this.className += "active-btn";
+    });
+  }
+}
+PageTransitions();
