3aa1be6bea28cf59c35343abd4fcbd25666d3704
lbferrand <lbferrand@gmail.com>
2022-03-26 13:29:37 -0400

    Added images and styles

--- app.js
+++ app.js
@@ -33,5 +33,12 @@ function PageTransitions() {
       element.classList.add("active");
     }
   });
+
+  //Toggle Theme
+  const themeBtn = document.querySelector(".theme-btn");
+  themeBtn.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+  });
 }
 PageTransitions();
